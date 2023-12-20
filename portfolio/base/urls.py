456e228153910e88a5c4d4a3beda0ec0025c45ab
Itsvenkey <137922952+Itsvenkey@users.noreply.github.com>
2023-12-20 19:12:44 +0530

    updating the files

--- portfolio/base/urls.py
+++ portfolio/base/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.hello)
+    path('',views.hello),
+    path('linkedin/',views.linkedin),
+    path('github/',views.github),
+    path('intro-to-database-certificate/',views.dbcerti),
+    path('contact-list-django-app-certificate/',views.certificate1),
+    path('version-control-certificate/',views.versioncontrol),
+    path('django-web-framework-certificate/',views.djangowebframework),
+    path('intro-to-backend-development-certificate/',views.introbackend),
+    path('programming-in-python-certificate/',views.pythonprogramming),
+    
 ]
--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
 def hello(request):
     return render(request,'base/home.html')
+
+def linkedin(request):
+    return redirect('https://www.linkedin.com/in/itsvenkey')
+
+def github(request):
+    return redirect('https://github.com/Itsvenkey')
+
+def dbcerti(reqeust):
+    return redirect("https://coursera.org/share/0aaa355dbc214b831da5ace5ca9647c7")
+
+def certificate1(reqeust):
+    return redirect("https://coursera.org/share/27721e6e2505276091df7590ef36ced3")
+
+def versioncontrol(reqeust):
+    return redirect("https://coursera.org/share/9b639f6ebc211d171b25123b82ff0bc9")
+
+def djangowebframework(reqeust):
+    return redirect('https://coursera.org/share/fd424804b4dea29b13919fdc478a23fb')
+
+def introbackend(request):
+    return redirect("https://coursera.org/share/06cc1ec2d8bec4fe056c87080277f017")
+
+def pythonprogramming(reqeust):
+    return redirect("https://coursera.org/share/aad8d4a9582b4e000c06c99a9a55da19")
